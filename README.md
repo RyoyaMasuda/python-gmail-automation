b36d4b9ca636a6c681838998802588694a0aa0dd
RyoyaMasuda <ryouya_masuda@unirita.co.jp>
2024-09-03 17:58:42 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # python-gmail-automation
 PythonでGmailを1件1件一斉送信するスクリプト
+
+# 事前準備
+- Google 2段階認証の設定
+- Googleアプリパスワードの設定
+
+## Google2段階認証の設定
+[2段階認証プロセスを有効にする](https://support.google.com/accounts/answer/185839?hl=ja&co=GENIE.Platform%3DDesktop&oco=0)を参照し、2段階認証を設定してください。
+
+## Googleアプリパスワードの設定
+[アプリ パスワードを作成、管理する](https://myaccount.google.com/apppasswords)を参照し、Googleアプリパスワードを作成してください。 
+※16桁のパスワードは一度しか表示されないので注意してください。
+
+# 使用方法
+1. このリポジトリをクローンしてください。
+2. Pythonの環境を3.9.6に設定します。([Pyenv](https://github.com/pyenv/pyenv)等のバージョン管理ツールを使用することをお勧めします。)
+3. 仮想環境の立ち上げ
+   1. ```python -m venv .venv```で仮想環境を立ち上げます。
+   2. ```source .venv/bin/activate```で仮想環境の有効化→ターミナルのプロンプトの横に`(.venv)`と表示されているか確認します。
+   3. ```pip install -r requirements.txt```でライブラリをインストールします。
+4. `.env.template`の名前を変更して`.env`にします。
+5. `.env`ファイルの`GMAIL_ADDRESS=`に自分のGmailアドレス、`GOOGLE_APP_PASSWORD=`に先ほど設定したGoogleアプリパスワードを入力します。(クオテーションは不要)
+6. sample.csvファイルの編集
+   1. 1行目(header)は`名前`と`メールアドレス`にします。
+   2. 2行目以降名前とメールアドレスを記載してください。
+7. mail.txtの編集
+   1. {Name}はsample.csvファイルに記載された名前が入力されるので{Name}以外の部分を変更してください。
+8. ターミナルから```python main.py```で実行してください。
---
+++ main.py
@@ -0,0 +1,62 @@
+import smtplib
+import csv
+from email.mime.text import MIMEText
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#メール送信のクラス
+class Send():
+    # 初期化
+    def __init__(self, AddressTo, subject, text):
+        self.password = os.getenv('GOOGLE_APP_PASSWORD')
+        self.AddressFrom = os.getenv('GMAIL_ADDRESS')
+        self.AddressTo = AddressTo
+        self.subject = subject
+        self.text = text
+        self.charset = "UTF-8"
+
+    def send(self):
+        #メールの主要設定
+        msg = MIMEText(self.text.encode(self.charset), 'plain', self.charset)
+        msg['From'] = self.AddressFrom
+        msg['To'] = self.AddressTo
+        msg['Subject'] = self.subject
+
+        #メールの詳細設定
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        # smtp.ehlo()でsmtp側にクライアント(このPythonソースコード)を認識させる。
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(self.AddressFrom, self.password)
+        smtp.send_message(msg)
+        smtp.close()
+
+def MailText(Name):
+    with open('mail.txt', 'r') as f:
+        content = f.read()
+
+    text = content.format(Name=Name)
+    return text
+
+if __name__ == '__main__':
+    #データの読み込み
+    file = 'addresslist.csv'  #パスを相対パスで指定
+    with open(file, 'r') as f:
+        date = csv.reader(f)
+        header = next(date)  #ヘッダーの読み込み
+
+        for row in date:
+            # 名前、アドレスをCSVから取得
+            Name = row[0]  #名前
+            Email = row[1]  #メールアドレス
+
+            # メール内容
+            AddressTo = Email
+            subject = "test送信"
+            text = MailText(Name)
+            mailer = Send(AddressTo, subject, text)
+            mailer.send()
